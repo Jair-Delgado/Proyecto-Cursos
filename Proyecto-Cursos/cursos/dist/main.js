d1fa18820d7146048eb79a867dbee9282f81d463
Jair-Delgado <102534932+Jair-Delgado@users.noreply.github.com>
2023-01-05 09:33:14 -0500

    Add files via upload

---
+++ Proyecto-Cursos/cursos/dist/main.js
@@ -0,0 +1,111 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let students = [];
+let teachers = [];
+let activities = [];
+let gradeBooksSetup = [];
+let grades = [];
+let areas = [];
+let summaryGrades = [];
+function addStudent() {
+    let currentStudent = {
+        fullName: readFormHtml("fullName"),
+        identification: parseInt(readFormHtml("identification")),
+        direction: readFormHtml("direction"),
+        enrollment: parseInt(readFormHtml("enrollment")),
+        level: readFormHtml("level"),
+    };
+    students.push(currentStudent);
+    console.table(students);
+}
+function addTeacher() {
+    let currentTeacher = {
+        fullName: readFormHtml("fullName-teacher"),
+        identification: parseInt(readFormHtml("identification-teacher")),
+        direction: readFormHtml("direction-teacher"),
+        title: readFormHtml("title-teacher"),
+        area: readFormHtml("area-teacher"),
+    };
+    teachers.push(currentTeacher);
+    console.table(teachers);
+}
+function addActivity() {
+    let currentActivity = {
+        name: readFormHtml("name-activity"),
+    };
+    activities.push(currentActivity);
+    console.table(activities);
+    initSelect();
+}
+function addGradeBookSetup() {
+    let currentGradeBookSetup = {
+        value: readFormHtml("value-gradebook"),
+        course: readFormHtml("course-gradebook"),
+        activity: readFormHtml("activity-gradebook"),
+        maximunGrade: parseInt(readFormHtml("maximun-gradebook")),
+    };
+    gradeBooksSetup.push(currentGradeBookSetup);
+    console.table(gradeBooksSetup);
+}
+function addGrade() {
+    let currentGrade = {
+        gradeName: readFormHtml("grade-name"),
+    };
+    grades.push(currentGrade);
+    console.table(grades);
+    initSelect();
+}
+function addArea() {
+    let currentArea = {
+        areaName: readFormHtml("area-name"),
+    };
+    areas.push(currentArea);
+    console.table(areas);
+    initSelect();
+}
+function addSummaryGrade() {
+    let currentSummaryGrade = {
+        studentName: readFormHtml("fullName"),
+        teacher: readFormHtml("fullName-teacher"),
+        grade: readFormHtml("grade-summary"),
+        score: parseInt(readFormHtml("score-summary")),
+    };
+    summaryGrades.push(currentSummaryGrade);
+    console.table(summaryGrades);
+    initSelect();
+}
+function readFormHtml(id) {
+    return document.getElementById(id).value;
+}
+function initSelect() {
+    let area = document.getElementById("area-teacher");
+    areas.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.areaName;
+        option.text = value.areaName,
+            area.add(option);
+    });
+    let grade = document.getElementById("course-gradebook");
+    grades.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.gradeName;
+        option.text = value.gradeName,
+            grade.add(option);
+    });
+    let activityGradebook = document.getElementById("activity-gradebook");
+    document.querySelectorAll("#activity-gradebook option").forEach(option => option.remove());
+    activities.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.name,
+            option.text = value.name,
+            activityGradebook.add(option);
+    });
+    let summaryGrade = document.getElementById("student-summary");
+    students.forEach((value) => {
+        let option = document.createElement("option");
+        option.value = value.fullName,
+            option.text = value.fullName,
+            summaryGrade.add(option);
+    });
+}
+initSelect();
